c8519b957eabb735f3afd47d801fa1a1794c8197
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-09-30 15:24:50 -0300

    feat: Add Server-Side Cookie

--- ignite-reactjs-auth-frontend/src/pages/index.tsx
+++ ignite-reactjs-auth-frontend/src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
+import { parseCookies } from "nookies";
 import { AuthContext } from "../context/AuthContext";
 
 import styles from "../styles/home.module.css";
@@ -29,3 +31,20 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
