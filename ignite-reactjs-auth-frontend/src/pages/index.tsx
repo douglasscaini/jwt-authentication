97a844b22c0954056203249351992e34375b0e08
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-10-04 14:50:16 -0300

    chore: Styled Sign-In

--- ignite-reactjs-auth-frontend/src/pages/index.tsx
+++ ignite-reactjs-auth-frontend/src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { withSSRQuest } from "../utils/withSSRGuest";
 
-import styles from "../styles/home.module.css";
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -23,12 +22,58 @@ export default function Home() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        onSubmit={handleSubmit}
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+            />
+          </FormControl>
 
-      <button type="submit">Entrar</button>
-    </form>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" colorScheme="pink" mt="6" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
